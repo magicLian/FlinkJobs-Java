6c735ab7c2b4048e07180b5c2f448702ade23dba
lian.zhe <lian.zhe@advantech.com.cn>
2021-12-09 14:57:14 +0800

    add randomIntSource

--- src/main/java/com/adv/flinkjobs/source/RandomIntSourceTest.java
+++ src/main/java/com/adv/flinkjobs/source/RandomIntSourceTest.java
@@ -1,18 +1,39 @@
 package com.adv.flinkjobs.source;
 
 import com.adv.flinkjobs.source.customSource.RandomIntSourceFunction;
+import com.adv.models.RandomIntOutput;
+import org.apache.flink.api.common.eventtime.*;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.time.Duration;
+
 public class RandomIntSourceTest {
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(4);
+        env.setParallelism(1);
+
         Integer limit = 30;
 
-        DataStream<Integer> inputStream = env.addSource(new RandomIntSourceFunction(limit));
+        //add source
+        DataStream<RandomIntOutput> inputStream = env.addSource(new RandomIntSourceFunction(limit));
 
-        inputStream.print();
+        //make timestamps and watermark
+        DataStream<RandomIntOutput> timestampsStream = inputStream.assignTimestampsAndWatermarks(
+            WatermarkStrategy
+                .<RandomIntOutput>forBoundedOutOfOrderness(Duration.ofSeconds(3))
+                .withTimestampAssigner(
+                    new SerializableTimestampAssigner<RandomIntOutput>() {
+                        @Override
+                        public long extractTimestamp(RandomIntOutput element, long recordTimestamp) {
+                            return element.getTs();
+                        }
+                    }
+                )
+        );
+
+        inputStream.print("source");
+        timestampsStream.print("timestamp");
 
         env.execute();
     }
--- src/main/java/com/adv/flinkjobs/source/customSource/RandomIntSourceFunction.java
+++ src/main/java/com/adv/flinkjobs/source/customSource/RandomIntSourceFunction.java
@@ -1,10 +1,12 @@
 package com.adv.flinkjobs.source.customSource;
 
+import com.adv.models.RandomIntOutput;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
+import java.util.Date;
 import java.util.Random;
 
-public class RandomIntSourceFunction implements SourceFunction<Integer> {
+public class RandomIntSourceFunction implements SourceFunction<RandomIntOutput> {
     private static final long serialVersionUID = 1L;
 
     private Integer limit;
@@ -20,10 +22,10 @@ public class RandomIntSourceFunction implements SourceFunction<Integer> {
     }
 
     @Override
-    public void run(SourceContext<Integer> ctx) throws Exception {
+    public void run(SourceContext<RandomIntOutput> ctx) throws Exception {
         while (isRunning) {
             Random random = new Random();
-            ctx.collect(random.nextInt(this.limit));
+            ctx.collect(new RandomIntOutput(random.nextInt(this.limit), new Date().getTime()));
 
             Thread.sleep(1000);
         }
